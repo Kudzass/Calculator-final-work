ebbcd4f0492b9f00fb6911b545861dc5ad392d59
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-01-09 00:43:20 +0200

    Get the display element and all buttons

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ let currentNumber = "0";
 let previousNumber = null;
 let operator = null;
 let shouldResetDisplay = false;
+
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".button");
